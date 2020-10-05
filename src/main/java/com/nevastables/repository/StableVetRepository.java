c56e3b5364a83a986eccd4ffa1b7807888017dcc
AnneKsatn <AnneKsatn@gmail.com>
2020-10-05 22:43:46 +0300

    Сделана отправка ветеринарных процедур на Ionic

--- src/main/java/com/nevastables/repository/StableVetRepository.java
+++ src/main/java/com/nevastables/repository/StableVetRepository.java
@@ -16,4 +16,6 @@ import java.util.List;
 public interface StableVetRepository extends JpaRepository<StableVet, Long> {
 
     List<StableVet> findAllByStableVetInfoId(Long stableVetInfoId);
+
+    List<StableVet> findAllByHorseId(Long stableVetInfoId);
 }
